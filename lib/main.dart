4ad2b795079fd1a1028d6fba23dbf7f9300c63ac
vinhdohoang <vinhdo0913@gmail.com>
2023-09-05 23:19:00 +0700

    add landscape for mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:klondike/widget/restart.dart';
 import 'package:klondike/widget/winning.dart';
 import 'klondike_game.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
   runApp(
     GameWidget<KlondikeGame>.controlled(
       gameFactory: KlondikeGame.new,
       mouseCursor: SystemMouseCursors.grab,
-      overlayBuilderMap: {'Restart': (_, game) => Restart(game: game),
-      'Winning': (_, game) => Winning(game: game)},
+      overlayBuilderMap: {
+        'Restart': (_, game) => Restart(game: game),
+        'Winning': (_, game) => Winning(game: game)
+      },
       initialActiveOverlays: const ['Restart'],
     ),
   );
